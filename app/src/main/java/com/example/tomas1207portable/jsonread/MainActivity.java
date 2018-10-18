11b6d6a9fae03790798d7c6a641bceeb724e14dc
tomas1207 <tomasfernandes98@gmail.com>
2018-10-18 19:18:12 +0100

    Stream return null

--- app/src/main/java/com/example/tomas1207portable/jsonread/MainActivity.java
+++ app/src/main/java/com/example/tomas1207portable/jsonread/MainActivity.java
@@ -3,18 +3,28 @@ package com.example.tomas1207portable.jsonread;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private TextView txt;
+    private ArrayList<String> streamArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.Login_MainScreen);
+        streamArray = new ArrayList<>();
+       new GetContacts().execute();
+
     }
     private class GetContacts extends AsyncTask<Void, Void, Void> {
         @Override
@@ -37,11 +47,14 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject jsonObj = new JSONObject(jsonStr);
 
                     // Getting JSON Array node
-                    JSONArray contacts = jsonObj.getJSONArray("Streams");
+                    JSONArray contacts = jsonObj.getJSONArray("streams");
 
                     // looping through All Contacts
                     for (int i = 0; i < contacts.length(); i++) {
                         JSONObject c = contacts.getJSONObject(i);
+                        String nome = c.getString("stream");
+                        Log.w("Info","Json:"+ nome);
+                        streamArray.add(nome);
 
                     }
                 } catch (final JSONException e) {
@@ -64,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(),
                                 "Couldn't get json from server. Check LogCat for possible errors!",
                                 Toast.LENGTH_LONG).show();
+
                     }
                 });
             }
@@ -74,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
+            txt.setText(streamArray.toString());
 
         }
     }
