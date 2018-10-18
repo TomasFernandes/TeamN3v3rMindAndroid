0a5904e81e661449acfde99c159868fa8e23641f
tomas1207 <tomasfernandes98@gmail.com>
2018-10-18 19:39:18 +0100

    Post on Server

--- app/src/main/java/com/example/tomas1207portable/jsonread/MainActivity.java
+++ app/src/main/java/com/example/tomas1207portable/jsonread/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,20 +13,32 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txt;
+    private Button bnt;
     private ArrayList<String> streamArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt = findViewById(R.id.Login_MainScreen);
+        bnt = findViewById(R.id.PostOnData);
         streamArray = new ArrayList<>();
-       new GetContacts().execute();
-
+        new GetContacts().execute();
+        bnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new postOnServer().execute();
+            }
+        });
     }
     private class GetContacts extends AsyncTask<Void, Void, Void> {
         @Override
@@ -52,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                     // looping through All Contacts
                     for (int i = 0; i < contacts.length(); i++) {
                         JSONObject c = contacts.getJSONObject(i);
+
                         String nome = c.getString("stream");
                         Log.w("Info","Json:"+ nome);
                         streamArray.add(nome);
@@ -90,6 +105,52 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(result);
             txt.setText(streamArray.toString());
 
+        }
+    }
+    private class postOnServer extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            URL url = null;
+            try
+            {
+                url = new URL("http://www.tomasfernandes.pt/Rest/example/addalunos");
+
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                String urlParams = "nome=tomas&idade=25&turma=407&curso=DDM";
+                connection.setRequestMethod("POST");
+
+                connection.setDoOutput(true);
+                DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
+                dStream.writeBytes(urlParams);
+                dStream.flush();
+                dStream.close();
+
+                int respondeCode = connection.getResponseCode();
+
+            }
+            catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
         }
     }
 }
