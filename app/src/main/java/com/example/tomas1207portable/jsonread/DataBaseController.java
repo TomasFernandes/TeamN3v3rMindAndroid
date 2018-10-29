48a43f2e8db27e0cd88530acd04db37a0ba0bed4
Tomas1207 <tomasfernandes98@gmail.com>
2018-10-29 17:13:42 +0000

    Data Base Tester working

--- app/src/main/java/com/example/tomas1207portable/jsonread/DataBaseController.java
+++ app/src/main/java/com/example/tomas1207portable/jsonread/DataBaseController.java
@@ -1,5 +1,6 @@
 package com.example.tomas1207portable.jsonread;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,7 +11,8 @@ import android.widget.Toast;
 public class DataBaseController extends AppCompatActivity {
     private Button bnt_dataBaseTest;
     private TextView tv_dataBaseTest;
-    private DataBaseTester DataBaseTester1;
+    private DataBaseTester dataBaseTester;
+    private Boolean haveData = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +20,48 @@ public class DataBaseController extends AppCompatActivity {
         setContentView(R.layout.activity_data_base_controller);
         bnt_dataBaseTest=findViewById(R.id.Download_test);
         tv_dataBaseTest=findViewById(R.id.DataBase_test);
+        new DataBaseTester().execute();
+
         bnt_dataBaseTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: Make a server conn whit outter class to see how to make:P
-                if (DataBaseTester1.getHaveData() == false) {
-                    Toast.makeText(DataBaseController.this, "No connection to server try again later", Toast.LENGTH_SHORT).show();
-                }
+
+            if(haveData){
+                Toast.makeText(DataBaseController.this, "Have data", Toast.LENGTH_SHORT).show();
+            }
+
             }
         });
+    }
+    public class DataBaseTester extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            haveData = false;
+            super.onCancelled();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+            String url = "http://tomasfernandes.pt/Beta/Streams.php";
+            String jsonStr = sh.makeServiceCall(url);
+            if(jsonStr == null){
+                haveData = false;
+            }
+            return null;
+        }
     }
 }
