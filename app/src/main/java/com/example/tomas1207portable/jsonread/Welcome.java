9180c0e2d691425bd0fee73c7c2eef2b285788c1
Tomas1207 <tomasfernandes98@gmail.com>
2018-11-23 09:08:38 +0000

    Welcome to Nav

--- app/src/main/java/com/example/tomas1207portable/jsonread/Welcome.java
+++ app/src/main/java/com/example/tomas1207portable/jsonread/Welcome.java
@@ -1,17 +1,26 @@
 package com.example.tomas1207portable.jsonread;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
-
+ private Button registar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SharedPreferences sharedPreferences = getSharedPreferences("JsonRead",MODE_PRIVATE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weclome);
         sharedPreferences.getBoolean("FirstTime",false);
-
+        registar = findViewById(R.id.Btn_Register);
+        registar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Welcome.this,Nav.class));
+            }
+        });
     }
 }
