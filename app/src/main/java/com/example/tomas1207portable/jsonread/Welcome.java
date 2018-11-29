c32feb050106fedd9b2ee707e16ab8b89a000830
Tomas1207 <tomasfernandes98@gmail.com>
2018-11-29 16:56:23 +0000

    Welcome from done. Pass encryter done(need test )

--- app/src/main/java/com/example/tomas1207portable/jsonread/Welcome.java
+++ app/src/main/java/com/example/tomas1207portable/jsonread/Welcome.java
@@ -4,8 +4,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Welcome extends AppCompatActivity {
  private Button registar;
@@ -16,6 +23,13 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_weclome);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean("FirstTime",false);
+        try {
+            Log.w("Pass",PassEncrypter.encrypt("Ola"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         registar = findViewById(R.id.Btn_Register);
         registar.setOnClickListener(new View.OnClickListener() {
@@ -25,4 +39,5 @@ public class Welcome extends AppCompatActivity {
             }
         });
     }
+
 }
