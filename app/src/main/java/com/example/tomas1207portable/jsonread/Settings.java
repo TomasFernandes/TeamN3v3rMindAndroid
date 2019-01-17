4aa1c94e05e01b60b5b3a17af6ff4451377683f2
pablo <pablo_espa@hotmail.es>
2019-01-17 14:40:36 +0000

    Settings 1.1

--- app/src/main/java/com/example/tomas1207portable/jsonread/Settings.java
+++ app/src/main/java/com/example/tomas1207portable/jsonread/Settings.java
@@ -1,7 +1,14 @@
 package com.example.tomas1207portable.jsonread;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
+
+import static android.Manifest.permission.INTERNET;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Switch dadosMoveis =  findViewById(R.id.switch2);
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (!mWifi.isConnected()&& !dadosMoveis.isChecked()) {
+        
+
+        }else
+            {
+
+            }
     }
+
 }
