3a34483ca15a46d94510196fbf7160052d103384
pablo <pablo_espa@hotmail.es>
2019-01-17 17:44:18 +0000

    Settings 1.2

--- app/src/main/java/com/example/tomas1207portable/jsonread/Settings.java
+++ app/src/main/java/com/example/tomas1207portable/jsonread/Settings.java
@@ -3,10 +3,13 @@ package com.example.tomas1207portable.jsonread;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import static android.Manifest.permission.INTERNET;
 
@@ -16,17 +19,27 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Switch dadosMoveis =  findViewById(R.id.switch2);
+        Switch dadosMoveis = findViewById(R.id.switch2);
         ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        final WifiManager  wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
 
-        if (!mWifi.isConnected()&& !dadosMoveis.isChecked()) {
-        
 
-        }else
-            {
+        dadosMoveis.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
 
+                if (isChecked == true && wifiManager.getWifiState() == WifiManager.WIFI_STATE_DISABLED) {
+                    wifiManager.setWifiEnabled(true);
+                    Toast.makeText(Settings.this,  wifiManager.getScanResults().toString(), Toast.LENGTH_SHORT).show();
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wifi ligado", Toast.LENGTH_LONG).show();
+                }
             }
+        });
     }
-
 }
+
+
